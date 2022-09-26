d6d8e52b7fa5f732b0907d840643817bbd2d3239
NikBel3476 <nikita.belkin3476@gmail.com>
2022-09-27 00:04:46 +0400

    chore(lint): update lint config

--- src-ui/main.tsx
+++ src-ui/main.tsx
@@ -1,14 +1,10 @@
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
 
-(document.getElementById('file_input') as HTMLInputElement).addEventListener('change', e => {
+/*(document.getElementById('file_input') as HTMLInputElement).addEventListener('change', e => {
 	console.log((e.target as HTMLInputElement).value);
-});
+});*/
 
-const rootElement = document.querySelector('#root');
-if (rootElement) {
-	const root = ReactDOM.createRoot(rootElement);
-	root.render(<App />);
-} else {
-	console.error('Root element not found');
-}
+const rootElement = document.querySelector('#root') as HTMLElement;
+const root = ReactDOM.createRoot(rootElement);
+root.render(<App />);
