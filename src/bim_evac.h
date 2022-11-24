6ae31752213ad0291ab7a1952993107972afc3f3
NikBel3476 <nikita.belkin3476@gmail.com>
2022-11-24 11:10:50 +0400

    feature: format bim_evac.c and bim_evac.h

--- src/bim_evac.h
+++ src/bim_evac.h
@@ -20,16 +20,24 @@
 #include "bim_graph.h"
 #include "logger.h"
 
-void    evac_def_modeling_step  (const bim_t *bim);
-void    evac_bim_ext_init       (const ArrayList *zones, const ArrayList *transits);
-void    evac_moving_step        (const bim_graph_t *graph, const ArrayList *zones, const ArrayList *transits);
+void evac_def_modeling_step(const bim_t *bim);
 
-void    evac_time_inc           (void);
-void    evac_time_reset         (void);
-double  evac_get_time_m         (void);
-double  evac_get_time_s         (void);
+void evac_bim_ext_init(const ArrayList *zones, const ArrayList *transits);
 
-void    evac_set_speed_max      (double val);
-void    evac_set_density_min    (double val);
-void    evac_set_density_max    (double val);
-void    evac_set_modeling_step  (double val);
+void evac_moving_step(const bim_graph_t *graph, const ArrayList *zones, const ArrayList *transits);
+
+void evac_time_inc(void);
+
+void evac_time_reset(void);
+
+double evac_get_time_m(void);
+
+double evac_get_time_s(void);
+
+void evac_set_speed_max(double val);
+
+void evac_set_density_min(double val);
+
+void evac_set_density_max(double val);
+
+void evac_set_modeling_step(double val);
