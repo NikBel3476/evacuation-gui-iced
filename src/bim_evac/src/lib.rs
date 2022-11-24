f9df47d0651b99043488a83073c8426e60ad42ca
NikBel3476 <nikita.belkin3476@gmail.com>
2022-11-24 22:57:10 +0400

    feature: init bim_evac library

---
+++ src/bim_evac/src/lib.rs
@@ -0,0 +1,18 @@
+// use libc::{c_double};
+//
+// static mut evac_speed_max: f64 = 100;
+// static mut evac_density_min: f64 = 0.1;
+// static mut evac_density_max: f64 = 5;
+// static mut evac_modeling_step: f64 = 0.01;
+// static mut evac_time: f64 = 0;
+
+// #[no_mangle]
+// pub extern "C" fn evac_def_modeling_step(bim: *mut bim_t) {
+
+// }
+
+// #[no_mangle]
+// pub extern "C" fn velocity(v0: c_dobule, a: c_double, d: c_double, d0: c_double) {
+//     return v0 * (1.0 - a * log(d / d0));
+// }
+
