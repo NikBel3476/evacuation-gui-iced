363f19efcfcd12c9c8372e51213059a72c14405b
NikBel3476 <nikita.belkin3476@gmail.com>
2022-12-27 15:12:28 +0400

    fix: make triangulate function as extern

--- thirdparty/triangle/triangle.h
+++ thirdparty/triangle/triangle.h
@@ -300,7 +300,7 @@ struct triangulateio {
 };
 
 #ifdef ANSI_DECLARATORS
-void triangulate(char *, struct triangulateio *, struct triangulateio *,
+extern void triangulate(char *, struct triangulateio *, struct triangulateio *,
                  struct triangulateio *);
 void trifree(VOID *memptr);
 #else /* not ANSI_DECLARATORS */
