df857c675035e690d91d5547b0c7e015c8f72927
NikBel3476 <nikita.belkin3476@gmail.com>
2022-12-31 19:27:42 +0400

    feature: add styles to buttons

--- src-ui/pages/MainPage/MainPage.tsx
+++ src-ui/pages/MainPage/MainPage.tsx
@@ -29,35 +29,59 @@ const MainPage: FC<mainPageProps> = () => {
 
 	return (
 		<main>
-			<h1 className="text-3xl font-bold">Main page</h1>
 			<header>
+				<h1 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+					Main page
+				</h1>
 				<nav>
 					<ul className={styles.linkList}>
 						<li>
-							<Link to="configuration">Configuration page</Link>
+							<Link
+								className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+								to="configuration"
+							>
+								Configuration page
+							</Link>
 						</li>
 						<li>
-							<button onClick={handleOpenConfigurationButtonClick}>
+							<button
+								className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+								onClick={handleOpenConfigurationButtonClick}
+							>
 								Открыть окно настроек
 							</button>
 						</li>
 						<li>
-							<button onClick={handleOpenConfigurationRescriptButtonClick}>
+							<button
+								className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+								onClick={handleOpenConfigurationRescriptButtonClick}
+							>
 								Открыть окно настроек(Rescript)
 							</button>
 						</li>
 						<li>
-							<button onClick={handleOpenPeopleTrafficButtonClick}>
+							<button
+								className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+								onClick={handleOpenPeopleTrafficButtonClick}
+							>
 								Открыть окно моделирования эвакуации
 							</button>
 						</li>
 						<li>
-							<button onClick={handleOpenBuildingViewButtonClick}>
+							<button
+								className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+								onClick={handleOpenBuildingViewButtonClick}
+							>
 								Открыть окно просмотра модели здания
 							</button>
 						</li>
 						<li>
-							<button onClick={handleBimStartButtonClick}>Запустить симуляцию</button>
+							<button
+								className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+								onClick={handleBimStartButtonClick}
+							>
+								Запустить симуляцию
+							</button>
 						</li>
 					</ul>
 				</nav>
