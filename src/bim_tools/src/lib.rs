661323793b7d1cbd911fdd5c802f868336cc853c
NikBel3476 <nikita.belkin3476@gmail.com>
2023-01-31 12:58:20 +0400

    feature: create bim_tools_new function for rust

--- src/bim_tools/src/lib.rs
+++ src/bim_tools/src/lib.rs
@@ -1,10 +1,9 @@
-#![allow(non_camel_case_types)]
-
-use bim_json_object::{bim_element_sign_t_rust, bim_json_object_t_rust, uuid_t};
+use bim_json_object::{bim_element_sign_t_rust, bim_json_object_t_rust, uuid_t, BimElementSign};
 use bim_polygon_tools::{
 	geom_tools_is_intersect_line_rust, geom_tools_length_side_rust, geom_tools_nearest_point_rust,
-	line_t, point_t, polygon_t,
+	line_t, point_t, polygon_t, polygon_t_rust,
 };
+use json_object::BuildingStruct;
 use libc::{c_char, c_double, c_int};
 use std::cmp::Ordering;
 use std::ffi::CString;
@@ -34,6 +33,35 @@ pub struct bim_transit_t {
 	pub sign: u8,
 	/// Количество связанных с текущим элементов
 	pub numofoutputs: u8,
+	/// Признак посещения элемента
+	pub is_visited: bool,
+	/// Признак недоступности элемента для движения
+	pub is_blocked: bool,
+}
+
+/// Структура, расширяющая элемент DOOR_*
+#[derive(Debug, Clone)]
+pub struct bim_transit_t_rust {
+	/// UUID идентификатор элемента
+	pub uuid: String,
+	/// Внутренний номер элемента
+	pub id: u64,
+	/// Название элемента
+	pub name: String,
+	/// Массив UUID элементов, которые являются соседними
+	pub outputs: Vec<String>,
+	/// Полигон элемента
+	pub polygon: polygon_t_rust,
+	/// Высота элемента
+	pub size_z: f64,
+	/// Уровень, на котором находится элемент
+	pub z_level: f64,
+	/// Ширина проема/двери
+	pub width: f64,
+	/// Количество людей, которые прошли через элемент
+	pub no_proceeding: f64,
+	/// Тип элемента
+	pub sign: BimElementSign,
 	/// Признак посещения элемента
 	pub is_visited: bool,
 	/// Признак недоступности элемента для движения
@@ -77,6 +105,41 @@ pub struct bim_zone_t {
 	pub is_safe: bool,
 }
 
+/// Структура, расширяющая элемент типа ROOM и STAIR
+#[derive(Debug, Clone)]
+pub struct bim_zone_t_rust {
+	/// UUID идентификатор элемента
+	pub uuid: String,
+	/// Внутренний номер элемента
+	pub id: u64,
+	/// Название элемента
+	pub name: String,
+	/// Полигон элемента
+	pub polygon: polygon_t_rust,
+	/// Массив UUID элементов, которые являются соседними
+	pub outputs: Vec<String>,
+	/// Высота элемента
+	pub size_z: f64,
+	/// Уровень, на котором находится элемент
+	pub z_level: f64,
+	/// Количество людей в элементе
+	pub number_of_people: f64,
+	/// Время достижения безопасной зоны
+	pub potential: f64,
+	/// Площадь элемента
+	pub area: f64,
+	/// Уровень опасности, % (0, 10, 20, ..., 90, 100)
+	pub hazard_level: u8,
+	/// Тип элемента
+	pub sign: BimElementSign,
+	/// Признак посещения элемента
+	pub is_visited: bool,
+	/// Признак недоступности элемента для движения
+	pub is_blocked: bool,
+	/// Признак безопасности зоны, т.е. в эту зону возможна эвакуация
+	pub is_safe: bool,
+}
+
 /// Структура, описывающая этаж
 #[repr(C)]
 pub struct bim_level_t {
@@ -94,17 +157,28 @@ pub struct bim_level_t {
 	pub numoftransits: u16,
 }
 
+/// Структура, описывающая этаж
+pub struct bim_level_t_rust {
+	/// Массив зон, которые принадлежат этажу
+	pub zones: Vec<bim_zone_t_rust>,
+	/// Массив переходов, которые принадлежат этажу
+	pub transits: Vec<bim_transit_t_rust>,
+	/// Название этажа
+	pub name: String,
+	/// Высота этажа над нулевой отметкой
+	pub z_level: f64,
+}
+
 /// Структура, описывающая здание
-#[repr(C)]
 pub struct bim_t_rust {
 	/// Массив уровней здания
-	pub levels: Vec<bim_level_t>,
+	pub levels: Vec<bim_level_t_rust>,
 	/// Название здания
 	pub name: String,
 	/// Список зон объекта
-	pub zones: Vec<bim_zone_t>,
+	pub zones: Vec<bim_zone_t_rust>,
 	/// Список переходов объекта
-	pub transits: Vec<bim_transit_t>,
+	pub transits: Vec<bim_transit_t_rust>,
 }
 
 // #[no_mangle]
@@ -306,7 +380,7 @@ pub extern "C" fn outside_init_rust(bim_json: *const bim_json_object_t_rust) -> 
 		polygon: std::ptr::null_mut(),
 		uuid: uuid_t {
 			x: uuid.try_into().unwrap_or_else(|v| {
-				panic!("Failed to convert vector to [char; 37]. Vector: {:?}", v)
+				panic!("Failed to convert vector to [char; 37]. Vector: {v:?}")
 			}),
 		},
 		z_level: 0.0,
@@ -355,11 +429,9 @@ pub extern "C" fn outside_init_rust(bim_json: *const bim_json_object_t_rust) -> 
 	}
 
 	outside.numofoutputs = num_of_outputs.try_into().unwrap_or_else(|e| {
-		panic!(
-			"Failed to convert usize to u32. usize: {}. Error: {:?}",
-			num_of_outputs, e
-		)
+		panic!("Failed to convert usize to u32. usize: {num_of_outputs}. Error: {e:?}")
 	});
+
 	outside.outputs = outputs.as_mut_ptr();
 	outside.is_blocked = false;
 	outside.is_visited = false;
@@ -403,10 +475,7 @@ pub extern "C" fn bim_tools_set_people_to_zone_rust(zone: *mut bim_zone_t, num_o
 	};
 
 	zone.numofpeople = f64::try_from(num_of_people).unwrap_or_else(|e| {
-		panic!(
-			"Failed to convert f32 to f64. f32: {}. Error: {:?}",
-			num_of_people, e
-		)
+		panic!("Failed to convert f32 to f64. f32: {num_of_people}. Error: {e:?}")
 	});
 }
 
@@ -434,25 +503,97 @@ pub extern "C" fn find_zone_callback_rust(value1: *mut bim_zone_t, value2: *mut 
 	1
 }
 
-/*fn bim_tools_new(bim_json: &bim_json_object) -> bim_t_rust {
-	let mut zones_list: Vec<bim_zone_t> = vec![];
-	let mut transits_list: Vec<bim_transit_t> = vec![];
+fn bim_tools_new(bim_json: &BuildingStruct) -> bim_t_rust {
+	let mut zones_list: Vec<bim_zone_t_rust> = vec![];
+	let mut transits_list: Vec<bim_transit_t_rust> = vec![];
+	let mut levels_list: Vec<bim_level_t_rust> = vec![];
 
-	let name = unsafe {
-		CString::from_raw(bim_json.name as *mut i8).into_string().unwrap_or_else(|e| {
-			panic!(
-				"Failed to convert CString to String. CString: {:?}. Error: {:?}",
-				bim_json.name, e
-			)
-		})
-	};
+	for level in &bim_json.levels {
+		let mut zones: Vec<bim_zone_t_rust> = vec![];
+		let mut transits: Vec<bim_transit_t_rust> = vec![];
 
+		for build_element in &level.build_elements {
+			let id = build_element.id.parse().unwrap_or_else(|e| {
+				panic!(
+					"Failed to parse id to u64. id: {}. Error: {:?}",
+					build_element.id, e
+				)
+			});
+			let uuid = build_element.uuid.clone();
+			let name = build_element.name.clone();
+			let size_z = build_element.size_z;
+			let z_level = level.z_level;
+			let sign = match build_element.sign.as_str() {
+				"Room" => BimElementSign::ROOM,
+				"Staircase" => BimElementSign::STAIRCASE,
+				"DoorWay" => BimElementSign::DOOR_WAY,
+				"DoorWayInt" => BimElementSign::DOOR_WAY_INT,
+				"DoorWayOut" => BimElementSign::DOOR_WAY_OUT,
+				_ => BimElementSign::UNDEFINED,
+			};
+			let outputs = build_element.outputs.clone();
+			let polygon = polygon_t_rust {
+				points: build_element.xy[0].points.clone(),
+			};
+			// TODO: replace string on enum
+			match build_element.sign.as_str() {
+				"Room" | "Staircase" => {
+					let zone = bim_zone_t_rust {
+						id,
+						uuid,
+						name,
+						size_z,
+						z_level,
+						sign,
+						// FIXME: unsafe cast u64 to f64
+						number_of_people: build_element.number_of_people as f64,
+						outputs,
+						area: polygon.area(),
+						polygon,
+						is_blocked: false,
+						is_visited: false,
+						is_safe: false,
+						potential: f64::from(f32::MAX),
+						hazard_level: 0,
+					};
+					zones.push(zone.clone());
+					zones_list.push(zone);
+				}
+				"DoorWay" | "DoorWayOut" | "WoorWayInt" => {
+					let transit = bim_transit_t_rust {
+						id,
+						name,
+						uuid,
+						size_z,
+						z_level,
+						sign,
+						outputs,
+						polygon,
+						is_blocked: false,
+						is_visited: false,
+						no_proceeding: 0.0,
+						width: -1.0,
+					};
+					transits.push(transit.clone());
+					transits_list.push(transit);
+				}
+				_ => {}
+			}
+		}
 
+		let bim_level = bim_level_t_rust {
+			name: level.name.clone(),
+			z_level: level.z_level,
+			zones,
+			transits,
+		};
+		levels_list.push(bim_level);
+	}
 
 	bim_t_rust {
 		transits: transits_list,
 		zones: zones_list,
-		levels: ,
-		name
+		levels: levels_list,
+		name: bim_json.name_building.clone(),
 	}
-}*/
+}
