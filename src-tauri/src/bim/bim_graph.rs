0800864e2a60354adc7a184d6cbe50679a12e95e
NikBel3476 <nikita.belkin3476@gmail.com>
2023-02-17 21:09:10 +0400

    chore: remove unused code in bim_graph

--- src-tauri/src/bim/bim_graph.rs
+++ src-tauri/src/bim/bim_graph.rs
@@ -1,10 +1,4 @@
-#![allow(non_camel_case_types)]
-
-extern crate core;
-
-use super::bim_tools::{
-	bim_t_rust, bim_transit_t, bim_transit_t_rust, bim_zone_t, bim_zone_t_rust,
-};
+use super::bim_tools::{bim_t_rust, bim_transit_t_rust, bim_zone_t_rust};
 use libc::size_t;
 
 /// https://www.techiedelight.com/implement-graph-data-structure-c
@@ -56,72 +50,6 @@ pub fn bim_graph_new(bim: &bim_t_rust) -> bim_graph_t_rust {
 	graph_create(&edges, edges.len(), bim.zones.len())
 }
 
-pub fn bim_graph_new_test(bim: &bim_t_rust) -> *mut bim_graph_t {
-	let edges = graph_create_edges_rust(&bim.transits, &bim.zones);
-
-	graph_create_rust(
-		edges.as_ptr(),
-		bim.transits.len() as size_t,
-		bim.zones.len() as size_t,
-	)
-}
-
-/// Function to create an adjacency list from specified edges
-#[no_mangle]
-#[allow(clippy::not_unsafe_ptr_arg_deref)]
-pub extern "C" fn graph_create_rust(
-	edges: *const bim_edge_t,
-	edge_count: size_t,
-	node_count: size_t,
-) -> *mut bim_graph_t {
-	// initialize head pointer for all vertices
-	let mut graph_head: Vec<*mut bim_node_t> = vec![std::ptr::null_mut(); node_count];
-
-	let edges = unsafe { std::slice::from_raw_parts(edges, edge_count) };
-	// add edges to the directed graph one by one
-	for i in 0..edge_count {
-		let edge = &edges[i];
-		// get the source and destination vertex
-		let src = edge.src;
-		let dest = edge.dest;
-		let eid = edge.id;
-
-		// 1. allocate a new node of adjacency list from `src` to `dest`
-		let mut new_node = bim_node_t {
-			dest,
-			eid,
-			next: std::ptr::null_mut(),
-		};
-
-		// point new node to the current head
-		new_node.next = graph_head[src];
-
-		// point head pointer to the new node
-		graph_head[src] = Box::into_raw(Box::new(new_node));
-
-		// 2. allocate a new node of adjacency list from `dest` to `src`
-		let new_node_dest_to_src = bim_node_t {
-			dest: src,
-			eid,
-			// point new node to the current head
-			next: graph_head[dest],
-		};
-
-		// change head pointer to point to the new node
-		graph_head[dest] = Box::into_raw(Box::new(new_node_dest_to_src));
-	}
-
-	// allocate storage for the graph data structure
-	let graph = bim_graph_t {
-		head: graph_head.as_mut_ptr(),
-		node_count,
-	};
-
-	std::mem::forget(graph_head);
-
-	Box::into_raw(Box::new(graph))
-}
-
 /// Function to create an adjacency list from specified edges
 pub fn graph_create(
 	edges: &[bim_edge_t_rust],
@@ -168,33 +96,6 @@ pub fn graph_create(
 	bim_graph_t_rust { head: graph_head }
 }
 
-pub fn graph_create_edges_rust(
-	list_doors: &[bim_transit_t_rust],
-	zones: &[bim_zone_t_rust],
-) -> Vec<bim_edge_t> {
-	let mut edges: Vec<bim_edge_t> = vec![];
-
-	for (i, transition) in list_doors.iter().enumerate() {
-		let mut ids = [0, zones.len()];
-		let mut j = 0usize;
-		for (k, zone) in zones.iter().enumerate() {
-			if equal_callback(zone, transition) && j != 2 {
-				ids[j] = k;
-				j += 1;
-			}
-		}
-
-		let edge = bim_edge_t {
-			id: i,
-			src: ids[0],
-			dest: ids[1],
-		};
-		edges.push(edge);
-	}
-
-	edges
-}
-
 pub fn graph_create_edges(
 	list_doors: &[bim_transit_t_rust],
 	zones: &[bim_zone_t_rust],
@@ -220,35 +121,6 @@ pub fn graph_create_edges(
 	}
 
 	edges
-}
-
-#[no_mangle]
-#[allow(clippy::not_unsafe_ptr_arg_deref)]
-pub extern "C" fn arraylist_equal_callback_rust(
-	value1: *const bim_zone_t,
-	value2: *const bim_transit_t,
-) -> i32 {
-	let element1 = unsafe {
-		value1
-			.as_ref()
-			.unwrap_or_else(|| panic!("Failed to get reference of value1"))
-	};
-
-	let element2 = unsafe {
-		value2
-			.as_ref()
-			.unwrap_or_else(|| panic!("Failed to get reference of value2"))
-	};
-
-	for i in 0..element1.numofoutputs {
-		let outputs =
-			unsafe { std::slice::from_raw_parts(element1.outputs, element1.numofoutputs as usize) };
-		if outputs[i as usize].x.eq(&element2.uuid.x) {
-			return 1;
-		}
-	}
-
-	0
 }
 
 pub fn equal_callback(zone: &bim_zone_t_rust, transit: &bim_transit_t_rust) -> bool {
