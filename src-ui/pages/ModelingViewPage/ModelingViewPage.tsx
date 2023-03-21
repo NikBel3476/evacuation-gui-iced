4df11921ef61e3e0d98bc2a4b647db8ded3c1c19
NikBel3476 <nikita.belkin3476@gmail.com>
2023-03-21 22:05:52 +0400

    feature: add canvas mouse handlers

--- src-ui/pages/ModelingViewPage/ModelingViewPage.tsx
+++ src-ui/pages/ModelingViewPage/ModelingViewPage.tsx
@@ -1,13 +1,24 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, {
+	MouseEventHandler,
+	useCallback,
+	useEffect,
+	useState,
+	WheelEventHandler
+} from 'react';
 import { Container, Graphics, Stage } from '@pixi/react';
 import { Graphics as PixiGraphics } from '@pixi/graphics';
 import buildingData from '../../peopleTraffic/udsu_b1_L4_v2_190701.json';
 import { View } from '../../BuildingView2D/application/view/View';
+import { Point } from 'pixi.js';
 
 const ModelingViewPage = () => {
 	const [scale, setScale] = useState<number>(5);
+	const [canMove, setCanMove] = useState<boolean>(false);
+	const [anchorCoordinates, setAnchorCoordiantes] = useState<Point>(new Point(0, 0));
 
-	useEffect(() => {}, []);
+	useEffect(() => {
+		console.log('render');
+	}, []);
 
 	const draw = useCallback((g: PixiGraphics) => {
 		g.clear();
@@ -32,9 +43,46 @@ const ModelingViewPage = () => {
 		View.drawBuildingRoomsPixi(g, buildingData.Level[0].BuildElement);
 	}, []);
 
+	const handleCanvasWheel: WheelEventHandler<HTMLCanvasElement> = event => {
+		switch (Math.sign(event.deltaY)) {
+			case -1:
+				setScale(x => x + 0.1);
+				break;
+			case +1:
+				setScale(x => x - 0.1);
+				break;
+		}
+	};
+
+	const handleCanvasMouseDown: MouseEventHandler<HTMLCanvasElement> = _ => {
+		setCanMove(true);
+	};
+
+	const handleCanvasMouseUp: MouseEventHandler<HTMLCanvasElement> = _ => {
+		setCanMove(false);
+	};
+
+	const handleCanvasMouseOut: MouseEventHandler<HTMLCanvasElement> = _ => {
+		setCanMove(false);
+	};
+
+	const handleCanvasMouseMove: MouseEventHandler<HTMLCanvasElement> = event => {
+		if (canMove) {
+			setAnchorCoordiantes(p => new Point(p.x + event.movementX, p.y + event.movementY));
+		}
+	};
+
 	return (
-		<Stage id="canvas" options={{ backgroundColor: 0xffffff }}>
-			<Container scale={scale}>
+		<Stage
+			id="canvas"
+			options={{ backgroundColor: 0xffffff }}
+			onWheel={handleCanvasWheel}
+			onMouseMove={handleCanvasMouseMove}
+			onMouseDown={handleCanvasMouseDown}
+			onMouseUp={handleCanvasMouseUp}
+			onMouseOut={handleCanvasMouseOut}
+		>
+			<Container scale={scale} x={anchorCoordinates.x} y={anchorCoordinates.y}>
 				<Graphics draw={draw} />
 			</Container>
 		</Stage>
